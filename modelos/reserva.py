a3f4e049328990e7567523d915c9a641b8ba188d
lexmarkcabrera-art <lexmarkcabrera@gmail.com>
2026-05-10 19:37:52 -0500

    Update reserva.py

--- modelos/reserva.py
+++ modelos/reserva.py
@@ -1,8 +1,84 @@
-# Este archivo permite relacionar
-# clientes con servicios mediante reservas.
+# =========================================================
+# Archivo: reserva.py
+# Descripción:
+# Este archivo permite manejar las reservas
+# realizadas por los clientes.
+# =========================================================
+
+from excepciones.errores import ReservaError
+from modelos.cliente import Cliente
+from modelos.servicio import Servicio
+
 
 class Reserva:
 
-    def __init__(self, cliente, servicio):
+    def __init__(
+        self,
+        cliente,
+        servicio,
+        duracion
+    ):
+
+        # Validaciones de seguridad
+        if not isinstance(cliente, Cliente):
+            raise ReservaError(
+                "Cliente inválido"
+            )
+
+        if not isinstance(servicio, Servicio):
+            raise ReservaError(
+                "Servicio inválido"
+            )
+
+        if duracion <= 0:
+            raise ReservaError(
+                "La duración debe ser mayor que cero"
+            )
+
         self.cliente = cliente
         self.servicio = servicio
+        self.duracion = duracion
+        self.estado = "Pendiente"
+
+    # Confirmar reserva
+    def confirmar(self):
+
+        self.estado = "Confirmada"
+
+    # Cancelar reserva
+    def cancelar(self):
+
+        self.estado = "Cancelada"
+
+    # Procesar reserva
+    def procesar_reserva(self):
+
+        costo = self.servicio.calcular_costo(
+            self.duracion
+        )
+
+        print("\n=========== RESERVA ===========")
+
+        print(
+            self.cliente.mostrar_informacion()
+        )
+
+        print(
+            self.servicio.descripcion()
+        )
+
+        print(
+            f"Duración: {self.duracion}"
+        )
+
+        print(
+            f"Costo: ${costo}"
+        )
+
+        print(
+            f"Estado: {self.estado}"
+        )
+
+        print(
+            "===============================\n"
+        )
