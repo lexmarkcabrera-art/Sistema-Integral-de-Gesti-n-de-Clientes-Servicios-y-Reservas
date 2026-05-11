71b412c4a6b7bda6ac1abe66b0abf233e2de51d1
lexmarkcabrera-art <lexmarkcabrera@gmail.com>
2026-05-10 19:36:30 -0500

    Update cliente.py

--- modelos/cliente.py
+++ modelos/cliente.py
@@ -1,9 +1,16 @@
+# =========================================================
+# Archivo: cliente.py
+# Descripción:
 # En este archivo se maneja toda la información
 # relacionada con los clientes del sistema.
+# También se aplican validaciones y encapsulación.
+# =========================================================
 
 from abc import ABC, abstractmethod
+from excepciones.errores import ClienteError
 
 
+# Clase abstracta Persona
 class Persona(ABC):
 
     @abstractmethod
@@ -11,12 +18,53 @@ class Persona(ABC):
         pass
 
 
+# Clase Cliente
 class Cliente(Persona):
 
     def __init__(self, nombre, correo, telefono):
+
+        # Encapsulación de atributos
         self.__nombre = nombre
         self.__correo = correo
         self.__telefono = telefono
 
+        # Validación automática
+        self.validar_datos()
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @property
+    def correo(self):
+        return self.__correo
+
+    @property
+    def telefono(self):
+        return self.__telefono
+
+    # Método encargado de validar los datos
+    def validar_datos(self):
+
+        if len(self.__nombre.strip()) < 3:
+            raise ClienteError(
+                "El nombre es demasiado corto"
+            )
+
+        if "@" not in self.__correo:
+            raise ClienteError(
+                "Correo inválido"
+            )
+
+        if not self.__telefono.isdigit():
+            raise ClienteError(
+                "El teléfono debe contener solo números"
+            )
+
+    # Método sobrescrito
     def mostrar_informacion(self):
-        return f"Cliente: {self.__nombre}"
+
+        return (
+            f"Cliente: {self.__nombre} | "
+            f"Correo: {self.__correo}"
+        )
